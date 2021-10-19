9e6cee676d71759bdf3cf0b5890a91ef146b7a6b
RudovEgor <egorka.ruda@mail.ru>
2021-10-19 11:45:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y, x;
+
+            y = 4; // определно заранее
+
+            Console.Write("Введите x = ");
+            x = Convert.ToDouble(Console.ReadLine()); // считываем с консоли
+
+            if ((x % y) == 0) // проверка деления без остатка
+                Console.WriteLine($"y = {x/y}");
+            else
+                Console.WriteLine("Введренное число не делится нацело ");
+
+            Console.ReadLine(); // задерживаем консоль для просмотра
+        }
+    }
+}
